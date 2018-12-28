3840c05bdc08a947cf967e4d2ebe44f293fd1b94
Mike Shultz <shultzm@gmail.com>
2018-12-27 23:25:03 -0700

    Fleshed out tests for SkatterBid

--- tests/consts.py
+++ tests/consts.py
@@ -6,11 +6,17 @@ BID_CONTRACT_NAME = 'SkatterBid'
 ENV_CONTRACT_NAME = 'Env'
 
 DURATION_1 = 60*60*24*14 # 2 weeks
+DURATION_2 = 60*60*24*31 # 31 days
 FILE_HASH_1 = '0x16c55d9e9ca5b673cafaa112195a5ad78ceb104e612ff2afbf34c233d6e7482b'
 FILE_SIZE_1 = 1024
+FILE_HASH_2 = '0xbf5c0efb83b4a3762058cfdf011d03d34c11d9721ad3e7ffc4b53d4287ddcc1b'
+FILE_SIZE_2 = 4096
 
 UINT_HASH_1 = Web3.sha3(text='whatever')
 UINT_VAL_1 = 123
 
 STR_HASH_1 = Web3.sha3(text='imastring')
 STR_VAL_1 = 'imavalue'
+
+ENV_ACCEPT_WAIT = Web3.sha3(text='acceptHoldDuration')
+ENV_DEFAULT_MIN_VALIDATIONS = Web3.sha3(text='defaultMinValidations')
--- tests/test_bids.py
+++ tests/test_bids.py
@@ -10,10 +10,28 @@ from .utils import (
 )
 from .consts import (
     BID_CONTRACT_NAME,
+    ENV_CONTRACT_NAME,
     FILE_HASH_1,
     FILE_SIZE_1,
     DURATION_1,
+    FILE_HASH_2,
+    FILE_SIZE_2,
+    DURATION_2,
+    ENV_ACCEPT_WAIT,
+    ENV_DEFAULT_MIN_VALIDATIONS,
 )
+
+
+def calculate_sway(min_valid, validations):
+    assert min_valid <= len(validations)
+    sway = 0.0
+    for v in validations:
+        if v is True:
+            sway += 1.0
+        else:
+            sway -= 1.0
+    return sway
+
 
 def test_standard_bid(web3, contracts):
     """ Test a simple bid with minimum validations set """
@@ -84,3 +102,215 @@ def test_bid_with_minvalidations(web3, contracts):
         minValidations,
     ) = bid.functions.getBid(evnt.args.bidId).call()
     assert minValidations == 5, "Invalid validationPool: {}".format(evnt.args.validationPool)
+
+def test_accept(web3, contracts):
+    """ Test accepting bids """
+    _, bidder, hoster, _, _, joe = get_accounts(web3)
+    bid = contracts.get(BID_CONTRACT_NAME)
+    env = contracts.get(ENV_CONTRACT_NAME)
+
+    # Bid
+    bid_hash = bid.functions.bid(
+        FILE_HASH_2,
+        FILE_SIZE_2,
+        DURATION_2,
+        int(1e16),
+        int(1e14)
+    ).transact(get_std_tx(bidder))
+
+    bid_receipt = web3.eth.waitForTransactionReceipt(bid_hash)
+    assert has_event(bid, 'BidSuccessful', bid_receipt), 'BidSuccessful event not found'
+
+    evnt = get_event(bid, 'BidSuccessful', bid_receipt)
+    bid_id = evnt.args.bidId
+
+    # Accept
+    accept_hash = bid.functions.accept(bid_id).transact(get_std_tx(hoster))
+    accept_receipt = web3.eth.waitForTransactionReceipt(accept_hash)
+    assert accept_receipt.status == 1, "accept failed"
+    assert has_event(bid, 'Accepted', accept_receipt), "Accepted event not found"
+
+    # Joe can not also accept before wait period is over
+    accept2_hash = bid.functions.accept(bid_id).transact(get_std_tx(joe))
+    accept2_receipt = web3.eth.waitForTransactionReceipt(accept2_hash)
+    assert accept2_receipt.status == 1, "accept failed"
+    assert has_event(bid, 'AcceptWait', accept2_receipt), "AcceptWait event not found"
+
+    # Time travel past set acceptHoldDuration
+    wait = env.functions.getuint(ENV_ACCEPT_WAIT).call()
+    web3.providers[0].make_request("evm_increaseTime", [wait])
+
+    # Joe can accept after wait period is over
+    accept3_hash = bid.functions.accept(bid_id).transact(get_std_tx(joe))
+    accept3_receipt = web3.eth.waitForTransactionReceipt(accept3_hash)
+    assert accept3_receipt.status == 1, "accept failed"
+    assert has_event(bid, 'Accepted', accept_receipt), "Accepted event not found"
+
+
+def test_pinned(web3, contracts):
+    """ Test a simple bid with minimum validations set """
+    _, bidder, hoster, _, _, jake = get_accounts(web3)
+    bid = contracts.get(BID_CONTRACT_NAME)
+
+    # Bid
+    bid_hash = bid.functions.bid(
+        FILE_HASH_2,
+        FILE_SIZE_2,
+        DURATION_2,
+        int(1e16),
+        int(1e14)
+    ).transact(get_std_tx(bidder))
+
+    bid_receipt = web3.eth.waitForTransactionReceipt(bid_hash)
+    assert has_event(bid, 'BidSuccessful', bid_receipt), 'BidSuccessful event not found'
+
+    evnt = get_event(bid, 'BidSuccessful', bid_receipt)
+    bid_id = evnt.args.bidId
+
+    # Accept
+    accept_hash = bid.functions.accept(bid_id).transact(get_std_tx(hoster))
+    accept_receipt = web3.eth.waitForTransactionReceipt(accept_hash)
+    assert accept_receipt.status == 1, "accept failed"
+    assert has_event(bid, 'Accepted', accept_receipt), "Accepted event not found"
+
+    # Rando can not pin after accepted by another
+    accept2_hash = bid.functions.accept(bid_id).transact(get_std_tx(jake))
+    accept2_receipt = web3.eth.waitForTransactionReceipt(accept2_hash)
+    assert accept2_receipt.status == 1, "accept failed"
+    assert has_event(bid, 'AcceptWait', accept2_receipt), "AcceptWait event not found"
+
+    # Pin
+    pin_hash = bid.functions.pinned(bid_id).transact(get_std_tx(hoster))
+    pin_receipt = web3.eth.waitForTransactionReceipt(pin_hash)
+    assert pin_receipt.status == 1, "pin failed"
+    assert has_event(bid, 'Pinned', pin_receipt), "Pinned event not found"
+
+
+def test_validation(web3, contracts):
+    """ Test the full validation process
+
+    Notes:
+    - Must win majority by minValidations
+    - A bid doesn't need to be accepted to pin, but if accepted, it can only be pinned by the user
+        who accepted the bid(if not expired).
+    - Bidder can not validate their own bid
+    - 
+    """
+    validator4, bidder, hoster, validator1, validator2, validator3 = get_accounts(web3)
+    bid = contracts.get(BID_CONTRACT_NAME)
+    env = contracts.get(ENV_CONTRACT_NAME)
+
+    # Bid
+    bid_hash = bid.functions.bid(
+        FILE_HASH_1,
+        FILE_SIZE_1,
+        DURATION_1,
+        int(1e18),  # 1 Ether
+        int(1e17)  # 0.1 Ether
+    ).transact(get_std_tx(bidder))
+
+    bid_receipt = web3.eth.waitForTransactionReceipt(bid_hash)
+    assert has_event(bid, 'BidSuccessful', bid_receipt), 'BidSuccessful event not found'
+
+    evnt = get_event(bid, 'BidSuccessful', bid_receipt)
+    bid_id = evnt.args.bidId
+
+    # Pin
+    pin_hash = bid.functions.pinned(bid_id).transact(get_std_tx(hoster))
+    pin_receipt = web3.eth.waitForTransactionReceipt(pin_hash)
+    assert pin_receipt.status == 1, "pin failed"
+    assert has_event(bid, 'Pinned', pin_receipt), "Pinned event not found"
+
+    # Validation #1
+    v1_hash = bid.functions.validate(bid_id).transact(get_std_tx(validator1))
+    v1_receipt = web3.eth.waitForTransactionReceipt(v1_hash)
+    assert v1_receipt.status == 1, "validation #1 tx failed"
+    assert has_event(bid, 'ValidationOcurred', v1_receipt), "Pinned event not found"
+
+    # Check the Validaton data before moving on
+    evnt = get_event(bid, 'ValidationOcurred', v1_receipt)
+    assert len(evnt.args) == 3, "Invalid argument count in ValidationOcurred"
+    assert evnt.args.bidId == bid_id
+    assert evnt.args.validator == validator1
+    assert evnt.args.isValid
+
+    # Validation #2 (invalid)
+    v2_hash = bid.functions.invalidate(bid_id).transact(get_std_tx(validator2))
+    v2_receipt = web3.eth.waitForTransactionReceipt(v2_hash)
+    assert v2_receipt.status == 1, "validation #2 tx failed"
+    assert has_event(bid, 'ValidationOcurred', v2_receipt), "Pinned event not found"
+
+    # Check the Validaton data before moving on
+    evnt2 = get_event(bid, 'ValidationOcurred', v2_receipt)
+    assert len(evnt2.args) == 3, "Invalid argument count in ValidationOcurred"
+    assert evnt2.args.bidId == bid_id
+    assert evnt2.args.validator == validator2
+    assert evnt2.args.isValid == False
+
+    # Validation #3 (valid)
+    v3_hash = bid.functions.validate(bid_id).transact(get_std_tx(validator3))
+    v3_receipt = web3.eth.waitForTransactionReceipt(v3_hash)
+    assert v2_receipt.status == 1, "validation #3 tx failed"
+    assert has_event(bid, 'ValidationOcurred', v3_receipt), "Pinned event not found"
+
+    # Check the Validaton data before moving on
+    evnt3 = get_event(bid, 'ValidationOcurred', v3_receipt)
+    assert len(evnt3.args) == 3, "Invalid argument count in ValidationOcurred"
+    assert evnt3.args.bidId == bid_id
+    assert evnt3.args.validator == validator3
+    assert evnt3.args.isValid
+
+    # Validation #4 (valid)
+    v4_hash = bid.functions.validate(bid_id).transact(get_std_tx(validator4))
+    v4_receipt = web3.eth.waitForTransactionReceipt(v4_hash)
+    assert v4_receipt.status == 1, "validation #3 tx failed"
+    assert has_event(bid, 'ValidationOcurred', v4_receipt), "Pinned event not found"
+
+    # Check the Validaton data before moving on
+    evnt4 = get_event(bid, 'ValidationOcurred', v4_receipt)
+    assert len(evnt3.args) == 3, "Invalid argument count in ValidationOcurred"
+    assert evnt4.args.bidId == bid_id
+    assert evnt4.args.validator == validator4
+    assert evnt4.args.isValid
+
+    validationCount = bid.functions.getValidationCount(bid_id).call()
+
+    # Verify the bid
+    (
+        bidder,
+        fileHash,
+        fileSize,
+        bidAmount,
+        validationPool,
+        duration,
+        minValidations,
+    ) = bid.functions.getBid(bid_id).call()
+    assert bidder == bidder
+    defaultMinValidations = env.functions.getuint(ENV_DEFAULT_MIN_VALIDATIONS).call()
+    assert minValidations == defaultMinValidations
+    assert minValidations <= validationCount
+
+    # Get all of the validations
+    validations = []
+    for i in range(0,validationCount):
+        (_, v_when, v_validator, v_isValid, v_paid) = bid.functions.getValidation(bid_id, i).call()
+        assert v_validator in [validator1, validator2, validator3, validator4]
+        validations.append(v_isValid)
+
+    # Calculate sway on test side
+    projected_sway = calculate_sway(minValidations, validations)
+
+    # Make sure sway is accurate
+    sway = bid.functions.validationSway(bid_id).call()
+    assert sway == projected_sway
+    assert sway != 0
+    majority_sway = float(minValidations) / float(sway)
+    assert -2.0 < majority_sway < 2.0, ("mojority sway should not more then 2 or less than -2 but "
+                                        "got {} (minValid: {}; sway: {})".format(
+                                            majority_sway,
+                                            minValidations,
+                                            sway))
+
+    # Make sure the bid is satisfied
+    satisfied = bid.functions.satisfied(bid_id).call()
+    assert satisfied, "bid should be satisfied but got: {}".format(satisfied)
