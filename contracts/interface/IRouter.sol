2a92621ea260ef8d12e8d5a05c29530ea0c406c8
Mike Shultz <shultzm@gmail.com>
2019-01-14 01:14:11 -0700

    renamed IScatterRouter to IRouter

--- contracts/interface/IRouter.sol
+++ contracts/interface/IRouter.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.0 <0.6.0;
 
-interface IScatterRouter {
+interface IRouter {
     function get(bytes32 _name) external view returns (address);
     function set(bytes32 _name, address _target) external;
 }
