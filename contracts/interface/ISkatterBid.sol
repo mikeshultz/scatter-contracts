b4c6b319270c292ee1a2f23c40187660c430fad4
Mike Shultz <shultzm@gmail.com>
2019-01-14 00:27:24 -0700

    reorganizing contracts

---
+++ contracts/interface/ISkatterBid.sol
@@ -0,0 +1,84 @@
+pragma solidity >=0.4.0 <0.6.0;
+
+interface ISkatterBid {
+
+    /* Are structs useful in interface?
+    struct Validation {
+        int32 bidId;
+        int32 when;
+        address verifier;
+        bool isValid;
+    }
+
+    struct Bid {
+        address bidder;
+        bytes32 fileHash;
+        int32 fileSize;
+        uint32 bidAmount;
+        uint32 validationPool;
+        int32 duration;
+        int32 accepted;
+        address hoster;
+        int32 pinned;
+        bool paid;
+        int32 minValidations;
+        Validation[] validations;
+    }*/
+
+    event BidSuccessful(
+        int indexed bidId,
+        address indexed bidder,
+        uint indexed bidValue,
+        bytes32 fileHash
+    );
+    event BidInvalid(bytes32 indexed fileHash, string reason);
+    event BidTooLow(bytes32 indexed fileHash);
+    event Accepted(int indexed bidId, address indexed hoster);
+    event AcceptWait(int waitLeft);
+    event Pinned(int indexed bidId, address indexed hoster, bytes32 fileHash);
+    event NotAcceptedByPinner(int indexed bidId, address indexed hoster);
+    event ValidationOcurred(int indexed bidId, address indexed validator, bool indexed isValid);
+
+    function getJob() external view returns (int, bytes32, int);
+    function getValidation(int bidId, int idx) external view returns (uint, address, bool, bool);
+    function getValidationCount(int bidId) external view returns (uint);
+
+    function isBidOpenForAccept(int bidId) external view returns (bool);
+    function isBidOpenForPin(int bidId) external view returns (bool);
+    function validationSway(int bidId) external view returns (uint);
+    function satisfied(int bidId) external view returns (bool);
+
+    function balance(address _address) external view returns (uint);
+    function balance() external view returns (uint);
+
+    function bid(
+        bytes32 fileHash,
+        int fileSize,
+        int duration,
+        uint bidValue,
+        uint validationPool
+    )
+    external
+    payable
+    returns (bool);
+
+    function bid(
+        bytes32 fileHash,
+        int fileSize,
+        int durationSeconds,
+        uint bidValue,
+        uint validationPool,
+        int minValidations
+    )
+    external
+    payable
+    returns (bool);
+
+    function accept(int bidId) external returns (bool);
+    function pinned(int bidId) external returns (bool);
+    function validate(int bidId) external;
+    function invalidate(int bidId) external;
+    function transfer(address payable _dest) external;
+    function withdraw() external;
+
+}
---
+++ contracts/lib/SkatterRewards.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.2;
+
+import './SafeMath.sol';
+import './Structures.sol';
+import '../interface/IBidStore.sol';
+
+library SkatterRewards {
+    using SafeMath for uint;
+
+    uint constant MAX_UINT = 2**256 - 1;
+
+    function getSplitAndRemainder(uint poolValue, uint validationCount)
+    public pure
+    returns (uint, uint)
+    {
+        return (
+            poolValue.div(validationCount),
+            poolValue.mod(validationCount)
+        );
+    }
+
+    function payValidators(address _store, int bidId, mapping(address => uint) storage sheet)
+    internal returns (uint)
+    {
+        IBidStore store = IBidStore(_store);
+        uint validationPool = store.getValidationPool(bidId);
+        uint validatorCount = store.getValidationCount(bidId);
+
+        uint totalPaid = 0;
+        uint split;
+        uint remainder;
+        (split, remainder) = getSplitAndRemainder(
+            validationPool,
+            validatorCount
+        );
+
+        for (uint i=0; i<validatorCount; i++)
+        {
+            address payable validator = store.getValidator(bidId, i);
+            require(store.setValidatorPaid(bidId, i), "set paid failed");
+            sheet[validator] += split;
+            totalPaid += split;
+        }
+
+        require(totalPaid <= validationPool, "invalid payouts");
+
+        return totalPaid;
+    }
+
+    function payHoster(address _store, int bidId, mapping(address => uint) storage sheet)
+    internal returns (bool)
+    {
+        IBidStore store = IBidStore(_store);
+        uint bidAmount = store.getBidAmount(bidId);
+        address hoster = store.getHoster(bidId);
+
+        require(store.setHosterPaid(bidId), "set paid failed");
+        sheet[hoster] += bidAmount;
+
+        return true;
+    }
+
+}
