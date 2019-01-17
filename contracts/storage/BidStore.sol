8a6ce6816f916f5b2ba5bd12092b7c16bb08323e
Mike Shultz <shultzm@gmail.com>
2019-01-16 20:44:14 -0700

    docstrings for BidStore!

--- contracts/storage/BidStore.sol
+++ contracts/storage/BidStore.sol
@@ -17,14 +17,21 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         _;
     }
 
+    /** constructor(address)
+     *  @dev initialize the contract
+     *  @param  _scatter    The address of the primary Scatter contract
+     */
     constructor(address _scatter) public {
         scatterAddress = _scatter;
     }
 
-    /**
-     * BidStore setter interface
+    /** addValidation(int, address payable, bool)
+     *  @dev Add a validation to a bid
+     *  @param  bidId       The ID of the bid to add the validation to
+     *  @param  _validator  The address of the validator
+     *  @param  _isValid    Whether or not the pin was marked valid
+     *  @return bool Success?
      */
-
     function addValidation(int bidId, address payable _validator, bool _isValid)
     external scatterOnly returns (bool)
     {
@@ -46,6 +53,12 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** setValidatorPaid(int, uint)
+     *  @dev Set the Validation as paid
+     *  @param  bidId   The ID of the bid to add the validation to
+     *  @param  idx     The index of the Validation to set paid
+     *  @return bool Success?
+     */
     function setValidatorPaid(int bidId, uint idx) external scatterOnly returns (bool)
     {
         if (validations[bidId][idx].when == 0)
@@ -57,6 +70,12 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** setHoster(int, address payable)
+     *  @dev Set the hoster for a bid
+     *  @param  bidId   The ID of the bid to add the validation to
+     *  @param  _hoster The address of the hoster account
+     *  @return bool Success?
+     */
     function setHoster(int bidId, address payable _hoster) external scatterOnly returns (bool)
     {
         if (bids[bidId].bidder == address(0))
@@ -68,6 +87,11 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** setHosterPaid(int)
+     *  @dev Set the bid as paid
+     *  @param  bidId   The ID of the bid to add the validation to
+     *  @return bool Success?
+     */
     function setHosterPaid(int bidId) external scatterOnly returns (bool)
     {
         if (bids[bidId].bidder == address(0) || bids[bidId].hoster == address(0))
@@ -79,6 +103,12 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** setAcceptNow(int, address payable)
+     *  @dev Set the bid as accepted and set the hoster
+     *  @param  bidId   The ID of the bid to add the validation to
+     *  @param  hoster  The address of the hoster account
+     *  @return bool Success?
+     */
     function setAcceptNow(int bidId, address payable hoster) external scatterOnly returns (bool)
     {
         if (bids[bidId].bidder == address(0))
@@ -91,7 +121,12 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
-
+    /** setPinned(int, address payable)
+     *  @dev Set the bid as pinned and set the hoster
+     *  @param  bidId   The ID of the bid to add the validation to
+     *  @param  hoster  The address of the hoster account
+     *  @return bool Success?
+     */
     function setPinned(int bidId, address payable hoster) external scatterOnly returns (bool)
     {
         if (bids[bidId].bidder == address(0))
@@ -109,6 +144,17 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** addBid(address payable, bytes32, int64, uint, uint, int16, uint)
+     *  @dev Add a bid to the Store
+     *  @param  _sender         The bidder
+     *  @param  fileHash        The IPFS file hash
+     *  @param  fileSize        The size of the file in bytes
+     *  @param  bidValue        The value of the bid to be paid to the hoster
+     *  @param  validationPool  The funds to be split by validators
+     *  @param  minValidations  Minimum validations for the bid
+     *  @param  durationSeconds The requested duration of the pin
+     *  @return uint            The ID for the bid created
+     */
     function addBid(address payable _sender, bytes32 fileHash, int64 fileSize, uint bidValue, 
                     uint validationPool, int16 minValidations, uint durationSeconds)
     external scatterOnly returns (int)
@@ -129,75 +175,141 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return bidId;
     }
 
-    /**
-     * BidStore getter interface
+    /** getBidCount()
+     *  @dev Return the total bids stored in this contract.
+     *  @return int     The total bids stored in the contract
      */
-
     function getBidCount() external view returns (int)
     {
         return bidCount;
     }
 
+    /** isPinned(int)
+     *  @dev Is a bid pinned?
+     *  @param bidId  The ID of the bid in question
+     *  @return bool  If the bid is pinned
+     */
     function isPinned(int bidId) external view returns (bool)
     {
         return (bids[bidId].pinned > 0);
     }
 
+    /** getPinned(int)
+     *  @dev Get the timestamp for the bid's pinning
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The timestamp of the pin
+     */
     function getPinned(int bidId) external view returns (uint)
     {
         return bids[bidId].pinned;
     }
 
+    /** getBidder(int)
+     *  @dev Return the address for the bidder
+     *  @param bidId            The ID of the bid in question
+     *  @return address payable The bidder address
+     */
     function getBidder(int bidId) external view returns (address payable)
     {
         return bids[bidId].bidder;
     }
 
+    /** getAccepted(int)
+     *  @dev Get the timestamp for the bid's acceptance
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The timestamp of the accept
+     */
     function getAccepted(int bidId) external view returns (uint)
     {
         return bids[bidId].accepted;
     }
 
+    /** getHoster(int)
+     *  @dev Get the hoster for the bid
+     *  @param bidId    The ID of the bid in question
+     *  @return address The address for the hoster
+     */
     function getHoster(int bidId) external view returns (address)
     {
         return bids[bidId].hoster;
     }
 
+    /** getFileHash(int)
+     *  @dev Get the fileHash for the bid
+     *  @param bidId    The ID of the bid in question
+     *  @return bytes32 The IPFS file hash given for the bid
+     */
     function getFileHash(int bidId) external view returns (bytes32)
     {
         return bids[bidId].fileHash;
     }
 
+    /** getFileSize(int)
+     *  @dev Get the fileSize for the bid
+     *  @param bidId  The ID of the bid in question
+     *  @return int64 The file size in bytes
+     */
     function getFileSize(int bidId) external view returns (int64)
     {
         return bids[bidId].fileSize;
     }
 
+    /** getDuration(int)
+     *  @dev Get the requested pin duration for the bid
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The pin duration in seconds
+     */
     function getDuration(int bidId) external view returns (uint)
     {
         return bids[bidId].duration;
     }
 
+    /** getValidationPool(int)
+     *  @dev Get the funds to be split between validators
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The pin validation pool fund total
+     */
     function getValidationPool(int bidId) external view returns (uint)
     {
         return bids[bidId].validationPool;
     }
 
+    /** getBidAmount(int)
+     *  @dev Get the funds to be paid to the hoster
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The amount of Ether to be paid to the hoster
+     */
     function getBidAmount(int bidId) external view returns (uint)
     {
         return bids[bidId].bidAmount;
     }
 
+    /** getMinValidations(int)
+     *  @dev The minimum validations set for a bid
+     *  @param bidId    The ID of the bid in question
+     *  @return int16   The amount of validations needed for a bid to be satisfied
+     */
     function getMinValidations(int bidId) external view returns (int16)
     {
         return bids[bidId].minValidations;
     }
 
+    /** getValidationCount(int)
+     *  @dev Get the total validations performed for a pin
+     *  @param bidId    The ID of the bid in question
+     *  @return uint    The total validations assigned to a pin
+     */
     function getValidationCount(int bidId) external view returns (uint)
     {
         return validations[bidId].length;
     }
 
+    /** getValidatorIndex(int, address payable)
+     *  @dev Get the Validation index of a specific validator account
+     *  @param bidId        The ID of the bid in question
+     *  @param _validator   The address of the validator to look for.
+     *  @return uint        The index for the validator in the Bid
+     */
     function getValidatorIndex(int bidId, address payable _validator) external view returns (uint)
     {
         if (bids[bidId].bidder == address(0))
@@ -216,11 +328,26 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return uint(-1);
     }
     
+    /** getValidator(int, uint)
+     *  @dev Return the validator address at a specifc index
+     *  @param bidId            The ID of the bid in question
+     *  @param idx              The Validation index to return
+     *  @return address payable The validator address at the index
+     */
     function getValidator(int bidId, uint idx) external view returns (address payable)
     {
         return validations[bidId][idx].validator;
     }
 
+    /** getValidation(int, uint)
+     *  @dev Return the key attributes for a Validation and an index
+     *  @param bidId    The ID of the bid in question
+     *  @param idx      The Validation index to return
+     *  @return uint    The timestamp of the validation
+     *  @return address The address of the validator
+     *  @return bool    Did the validator validate?
+     *  @return bool    Has the validator been paid?
+     */
     function getValidation(int bidId, uint idx) external view returns (
         uint,       // when
         address,    // validator
@@ -236,11 +363,22 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         );
     }
 
+    /** getValidationIsValid(int, uint)
+     *  @dev Did the specific validation mark the pin as valid?
+     *  @param bidId    The ID of the bid in question
+     *  @param idx      The Validation index to return
+     *  @return bool    Did the validator validate?
+     */
     function getValidationIsValid(int bidId, uint idx) external view returns (bool)
     {
         return validations[bidId][idx].isValid;
     }
 
+    /** bidExists(int)
+     *  @dev Does a bid exist at this ID?
+     *  @param bidId    The ID of the bid in question
+     *  @return bool    Does it exist?
+     */
     function bidExists(int bidId) external view returns (bool)
     {
         if (bids[bidId].bidder == address(0))
@@ -250,6 +388,17 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         return true;
     }
 
+    /** bidExists(int)
+     *  @dev Get the primary attributes of a Bid
+     *  @param bidId    The ID of the bid in question
+     *  @return address Address of the bidder
+     *  @return bytes32 IPFS File hash
+     *  @return int     File size in bytes
+     *  @return uint    The amount of the bid in wei
+     *  @return uint    The amount to be split by validators in wei
+     *  @return uint    The duration in seconds for the pin
+     *  @return int16   The minimum validations
+     */
     function getBid(int bidId) external view returns (
         address,
         bytes32,
@@ -271,16 +420,20 @@ contract BidStore is Owned {  // Also is IBidStore, but solc doesn't like that r
         );
     }
 
-    /**
-     * Admin interface
+    /** setScatter(address)
+     *  @dev Set the address for the Scatter contract
+     *  @param _newAddress The new address for the Scatter contract
      */
-
     function setScatter(address _newAddress) public ownerOnly
     {
         assert(_newAddress != address(0));
         scatterAddress = _newAddress;
     }
 
+    /** setBidCount(int)
+     *  @dev Set the bidCount sequence.  WARNING: This is for emergencies!
+     *  @param _bidCount The new bidCount value
+     */
     function setBidCount(int _bidCount) public ownerOnly
     {
         bidCount = _bidCount;
